455b8460a5f7e73fe0aaef4149ed490c5545b65c
Jiayew521endfalsegit config --global user.name Your <jiaye.wei@epfl.ch>
2023-10-04 11:30:48 +0200

    Add the textbook and directory for exercises in chapter 4
    First commit of the pcsc repo, add the textbook of this course, and add a directory for exercises in chapter 4 about pointers.

---
+++ solutions/chapter-04/ch-04-ex-01.cpp
@@ -0,0 +1,17 @@
+/*
+ * chapter-04-exercise-01.cpp
+ *
+ *
+ *
+ * Created on: Oct 4, 2023
+ * 	   Author: Jiaye Wei <jiaye.wei@epfl.ch>
+ */
+
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int 
+    return 0;
+}
